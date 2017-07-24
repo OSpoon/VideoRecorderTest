4be623d5b55142354f4c3bf7cb1f85d00297c4f1
zxin088@gmail.com <zxin088@gmail.com>
2017-07-24 18:54:31 +0800

    更新过滤文件

--- app/src/main/java/com/n22/util/encoder/Config.java
+++
@@ -1,6 +0,0 @@
-package com.n22.util.encoder;
-
-public class Config {
-    public static final String ORG_VALIDATE_CODE = "ABCD12345678901234567890";// 用于请求签名
-    public static String COM_ID = "HT20151001";// 机构验证码，用于请求签名
-}
